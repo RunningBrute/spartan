ca92ff3a459f5dc71a802a21d214e1dfcdabce83
Piotr Podusowski <podusowski@gmail.com>
2016-12-09 14:30:43 -0500

    more js cleanup

--- training/static/training/activity_map.js
+++ training/static/training/activity_map.js
@@ -1,13 +1,48 @@
-function render_activity_map(map_id, points)
-{
-    var map = L.map(map_id).setView([0, 0], 13);
+activityMap = {}
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+activityMap.makeGpsLayer = function(json) {
+    var points = json.map(function(point) { return [point.lon, point.lat]; });
+    var track = new ol.geom.LineString(points).transform('EPSG:4326', 'EPSG:3857');
 
-    var polyline_points = points.map(function (point){ return [point.lat, point.lon]; });
+    var featureLine = new ol.Feature({
+        geometry: track
+    });
 
-    var polyline = L.polyline(polyline_points, {color: 'red'}).addTo(map);
-    map.fitBounds(polyline.getBounds());
-}
+    var sourceLine = new ol.source.Vector({
+        features: [featureLine]
+    });
+
+    var styles = [
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: 'white',
+                width: 6
+            })
+        }),
+
+        new ol.style.Style({
+            stroke: new ol.style.Stroke({
+                color: '#3399ff',
+                width: 3
+            })
+        })
+    ]
+
+    var layer = new ol.layer.Vector({
+        source: sourceLine,
+        style: styles
+    });
+
+    return layer
+};
+
+activityMap.renderMap = function(target, layer) {
+    var osmLayer = new ol.layer.Tile({source: new ol.source.OSM()});
+
+    var map = new ol.Map({
+            layers: [osmLayer, layer],
+            target: target
+        });
+
+    map.getView().fit(layer.getSource().getExtent(), map.getSize());
+};
