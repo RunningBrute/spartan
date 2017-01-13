3288985afc01566e2f1946593f1184d4f3415e76
Piotr Podusowski <podusowski@gmail.com>
2017-01-13 07:43:26 -0500

    chart js refactor

--- training/static/training/activity_charts.js
+++ training/static/training/activity_charts.js
@@ -1,54 +1,6 @@
-var activityChart = {};
+var activityCharts = {};
 
-activityChart.distance = function(lat1, lon1, lat2, lon2) {
-    var lat1_in_rad = Math.PI * lat1 / 180;
-    var lat2_in_rad = Math.PI * lat2 / 180;
-    var theta = lon1 - lon2;
-    var theta_in_rad = Math.PI * theta / 180;
-    var dist = Math.sin(lat1_in_rad) * Math.sin(lat2_in_rad) + Math.cos(lat1_in_rad) * Math.cos(lat2_in_rad) * Math.cos(theta_in_rad);
-
-    dist = Math.acos(dist);
-    dist = dist * 180 / Math.PI;
-    dist = dist * 60 * 1.1515;
-    dist = dist * 1.609344 * 1000;
-
-    return dist;
-};
-
-activityChart.minMax = function(array) {
-    var max = -1;
-    var min = 10000;
-
-    for (i = 0; i < array.length; i++) {
-        if (array[i] > max) max = array[i];
-        if (array[i] < min) min = array[i];
-    }
-
-    return [min, max];
-}
-
-function get_single_intervals_time(points, time_data, interval_length_in_m)
-{
-    var coordinates = points.map(function (point){ return [point.lat, point.lon]; });
-    var distt = 0;
-    var time = 0.0;
-    var intervals_time = [];
-
-    for (i = 1; i < coordinates.length; i++)
-    {
-        lat1 = points[i-1].lat;
-        lon1 = points[i-1].lon;
-        lat2 = points[i].lat;
-        lon2 = points[i].lon;
-
-        distance = activityChart.distance(lat2, lon2, lat1, lon1);
-        time = points[i].time - points[i-1].time;
-    }
-
-    return intervals_time;
-}
-
-function setGlobalSettings()
+activityCharts.setGlobalSettings = function()
 {
     Chart.defaults.global.elements.rectangle.borderColor = 'rgba(207, 74, 8, 0.8)';
     Chart.defaults.global.elements.rectangle.backgroundColor = 'rgba(207, 74, 8, 0.1)';
@@ -62,9 +14,13 @@ function setGlobalSettings()
     Chart.defaults.global.legend.position = "right";
 }
 
-function render_charts(charts_id, pace1_id, pace2_id, points, average_hr, average_cad)
+activityCharts.render = function(charts_id, points)
 {
-    setGlobalSettings();
+    this.setGlobalSettings();
+
+    var isPresent = function(data) {
+        return data.length > 0 && data[0] != null;
+    };
 
     var hr_data = points.map(function (point){ return point.hr; });
     var cad_data = points.map(function (point){ return point.cad; });
@@ -79,90 +35,72 @@ function render_charts(charts_id, pace1_id, pace2_id, points, average_hr, averag
         return delta_time_in_ms / one_minute_in_ms;
     });
 
-    avg_hr_data = [];
-    avg_cad_data = [];
-
     var datasets = [];
 
-    if (average_hr)
-    {
-        datasets = datasets.concat(
-            [
-            {
-                borderColor: "rgba(153, 0, 0, 0.8)",
-                backgroundColor: "rgba(153, 0, 0, 0.1)",
-                label: "HR",
-                fill: false,
-                cubicInterpolationMode: "monotone",
-                pointRadius: 0,
-                data: hr_data
-            },
-            {
-                borderColor: "rgba(153, 0, 0, 0.4)",
-                backgroundColor: "rgba(153, 0, 0, 0)",
-                label: "Avg HR",
-                fill: false,
-                pointRadius: 0,
-                data: avg_hr_data
-            }
-            ]);
+    var makeAverage = function(data) {
+        sum = data.reduce(function(a, b) { return a + b; });
+        avg = sum / data.length;
 
-        avg_hr_data = new Array(hr_data.length);
-        avg_hr_data.fill(average_hr);
+        result = new Array(data.length);
+        result.fill(avg);
+
+        return result;
+    };
+
+    var makeDataset = function(data, label, color='rgba(153, 0, 0, 0.8)') {
+        return {
+            borderColor: color,
+            label: label,
+            fill: false,
+            pointRadius: 0,
+            data: data
+        };
+    };
+
+    if (isPresent(hr_data))
+    {
+        datasets = datasets.concat([
+            makeDataset(hr_data, "HR", 'rgba(153, 0, 0, 0.8)'),
+            makeDataset(makeAverage(hr_data), "AVG HR", 'rgba(153, 0, 0, 0.4)')
+        ]);
     }
 
-    if (average_cad)
+    if (isPresent(cad_data))
     {
-        avg_cad_data = new Array(cad_data.length);
-        avg_cad_data.fill(average_cad);
-
-        datasets = datasets.concat(
-            [
-                {
-                    borderColor: "rgba(0, 76, 153, 0.8)",
-                    label: "Cadence",
-                    fill: false,
-                    pointRadius: 0,
-                    data: cad_data
-                },
-                {
-                    borderColor: "rgba(0, 76, 153, 0.4)",
-                    label: "Avg cadence",
-                    fill: false,
-                    pointRadius: 0,
-                    data: avg_cad_data
-                },
-            ]);
+        datasets = datasets.concat([
+            makeDataset(cad_data, "CADENCE", 'rgba(0, 76, 153, 0.8)'),
+            makeDataset(makeAverage(cad_data), "AVG CADENCE", 'rgba(0, 76, 153, 0.4)')
+        ]);
     }
 
-        ctx = $(charts_id);
-        chart = new Chart(ctx, {
-            type: "line",
-            data: {
-                labels: time_data,
-                datasets: datasets
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        gridLines: {
-                            color: "rgba(207, 74, 8, 0.1)"
-                        },
-                        ticks: {
-                            beginAtZero: false
-                        }
-                    }],
-                    xAxes: [{
-                        gridLines: {
-                            color: "rgba(207, 74, 8, 0.1)"
-                        },
-                        ticks: {
-                            maxTicksLimit: 10,
-                            minRotation: 0,
-                            maxRotation: 0
-                        }
-                    }]
-                }
+    chart = new Chart($(charts_id), {
+        type: "line",
+        data: {
+            labels: time_data,
+            datasets: datasets
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    gridLines: {
+                        color: "rgba(207, 74, 8, 0.1)"
+                    },
+                    ticks: {
+                        beginAtZero: false
+                    }
+                }],
+                xAxes: [{
+                    gridLines: {
+                        color: "rgba(207, 74, 8, 0.1)"
+                    },
+                    ticks: {
+                        maxTicksLimit: 10,
+                        minRotation: 0,
+                        maxRotation: 0
+                    }
+                }]
             }
-        });
+        }
+    });
 }
+
